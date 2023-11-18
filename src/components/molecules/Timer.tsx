ac19f86e949fae2509b1f89a4b7606e1817651d1
Marius STEPHANY <marius.stephany@gmail.com>
2023-11-18 18:39:53 +0100

    :sparkles: Add gesture and clic event on timer

--- src/components/molecules/Timer.tsx
+++ src/components/molecules/Timer.tsx
@@ -1,5 +1,5 @@
 import formatTimer from "@/utils/formatTime";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
 import NumberText from "../atoms/NumberText";
 
 type TimerProps = {
@@ -7,59 +7,79 @@ type TimerProps = {
   children?: ReactNode;
 };
 
-export default function Timer({ addTime, children }: TimerProps) {
-  const [timer, setTimer] = useState(0);
-  const [timestampStarted, setTimestampStarted] = useState(0);
-  const [isStarted, setIsStarted] = useState(false);
-  const [isBeforeStarted, setIsBeforeStarted] = useState(false);
-  const [token, setToken] = useState(false);
-  const isTimerFocused = isStarted || isBeforeStarted;
+enum TimerState {
+  Ready = "READY",
+  Start = "START",
+  Stop = "STOP",
+}
 
-  useEffect(() => {
-    let interval: NodeJS.Timeout;
-    if (isStarted) {
-      interval = setInterval(() => {
-        setTimer(Date.now() - timestampStarted);
-      }, 12);
-    }
-    return () => {
-      clearInterval(interval);
-    };
-  }, [isStarted, timestampStarted]);
+enum GestureType {
+  Up = "UP",
+  Down = "DOWN",
+}
+
+export default function Timer({ addTime, children }: TimerProps) {
+  const [now, setNow] = useState<number | null>(null);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [timerState, setTimerState] = useState<TimerState>(TimerState.Stop);
+  const intervalRef = useRef<NodeJS.Timeout>();
+  const isTouchEventAvailable =
+    typeof window !== "undefined" && "ontouchstart" in window;
+
+  function readyTimer() {
+    setTimerState(TimerState.Ready);
+  }
+
+  function startTimer() {
+    setNow(Date.now());
+    setStartTime(Date.now());
+    setTimerState(TimerState.Start);
+
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  const stopTimer = useCallback(
+    function () {
+      setNow(Date.now());
+      setTimerState(TimerState.Stop);
+      clearInterval(intervalRef.current);
+      addTime(now && startTime ? now - startTime : 0);
+    },
+    [addTime, now, startTime]
+  );
+
+  const handleTimer = useCallback(
+    function (gesture: GestureType, isMouseEvent = false) {
+      if (isTouchEventAvailable && isMouseEvent) return;
+      if (typeof TouchEvent !== undefined)
+        if (timerState === TimerState.Stop) {
+          if (gesture === GestureType.Up) return;
+          readyTimer();
+        } else if (timerState === TimerState.Ready) {
+          if (gesture === GestureType.Down) return;
+          startTimer();
+        } else {
+          if (gesture === GestureType.Down) return;
+          stopTimer();
+        }
+    },
+    [stopTimer, timerState, isTouchEventAvailable]
+  );
 
   useEffect(() => {
     const keyUpHandler = (e: KeyboardEvent) => {
       if (e.code === "Space") {
         e.preventDefault();
+        handleTimer(GestureType.Up);
       }
-      if (!token) {
-        if (e.code === "Space") {
-          e.preventDefault();
-          // Start timer
-          setTimer(0);
-          setTimestampStarted(Date.now());
-          setIsStarted(true);
-          setIsBeforeStarted(false);
-        }
-      }
-      setToken(!token);
     };
 
     const keyDownHandler = (e: KeyboardEvent) => {
       if (e.code === "Space") {
         e.preventDefault();
-      }
-      if (isStarted) {
-        // Stop timer
-        const result = Date.now() - timestampStarted;
-        setTimer(result);
-        setTimestampStarted(0);
-        setIsStarted(false);
-        addTime(result);
-      } else {
-        if (e.code === "Space") {
-          setIsBeforeStarted(true);
-        }
+        handleTimer(GestureType.Down);
       }
     };
 
@@ -70,22 +90,31 @@ export default function Timer({ addTime, children }: TimerProps) {
       document.removeEventListener("keyup", keyUpHandler);
       document.removeEventListener("keydown", keyDownHandler);
     };
-  }, [isStarted, timestampStarted, token, addTime]);
+  }, [handleTimer, timerState]);
 
   return (
     <div
-      className={`flex flex-col justify-center items-center min-h-[400px] ${
-        isBeforeStarted
+      onTouchStart={() => handleTimer(GestureType.Down)}
+      onTouchEnd={() => handleTimer(GestureType.Up)}
+      onMouseDown={() => handleTimer(GestureType.Down, true)}
+      onMouseUp={() => handleTimer(GestureType.Up, true)}
+      onContextMenu={(e) => e.preventDefault()}
+      className={`flex flex-col justify-center items-center min-h-[400px] bg-gradient-to-b from-blue-600 to-blue-500
+      ${
+        timerState === TimerState.Ready
           ? "bg-gradient-to-b from-green-600 to-green-400"
           : "bg-gradient-to-b from-blue-600 to-blue-500"
-      } ${
-        isTimerFocused
+      }
+      ${
+        timerState === TimerState.Ready || timerState === TimerState.Start
           ? "fixed top-0 h-screen w-screen z-10 overflow-hidden"
           : "lg:h-[60%] "
       }`}
     >
-      <NumberText size="big">{formatTimer(timer)}</NumberText>
-      {!isTimerFocused && children}
+      <NumberText size="big">
+        {formatTimer(now && startTime ? now - startTime : 0)}
+      </NumberText>
+      {timerState === TimerState.Start ?? children}
     </div>
   );
 }
