5c9f730d432e27193d41d58045c63563334bb4eb
Marius STEPHANY <marius.stephany@gmail.com>
2023-12-06 17:17:07 +0100

    :lipstick: Fix preview height

--- src/components/organisms/PreviewPanel.tsx
+++ src/components/organisms/PreviewPanel.tsx
@@ -7,7 +7,7 @@ type PreviewPanelProps = {
 
 export default function PreviewPanel({ children }: PreviewPanelProps) {
   return (
-    <div className="flex justify-center align-middle w-full p-8">
+    <div className="flex justify-center align-middle w-full h-full p-8">
       <div className="w-full h-[calc(100%-60px)] bg-[#151E3F] rounded-2xl p-4">
         <div className="flex justify-center align-middle p-4  gap-4 rounded-2xl border-[1px] border-[#F6511D]">
           <span className="material-symbols-outlined text-white">
